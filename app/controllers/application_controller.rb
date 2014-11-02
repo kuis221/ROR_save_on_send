72adbb875b628ae491cf578f4397b06b46bb185b
Alexey Artamonov <i@useruby.com>
2014-11-02 16:32:58 +0100

    prevented ifinite redirect loop for registered user #81888350

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  # Overwriting the sign_in redirect path method
+  def after_sign_in_path_for(resource_or_scope)
+    flash[:alert] = nil
+    welcome_path
+  end
 end
