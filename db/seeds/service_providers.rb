b967216f16beb4394e5d66561af86d215832c907
Alexey Artamonov <i@useruby.com>
2014-11-10 15:27:17 +0100

    added additional service providers into seeds data #82043862

--- db/seeds/service_providers.rb
+++ db/seeds/service_providers.rb
@@ -1,3 +1,16 @@
-['Western Union', 'Xoom'].each do |name|
+[
+  'Western Union', 
+  'Xoom', 
+  'MoneyGram', 
+  'Transfast', 
+  'Ria', 
+  'Transferwise', 
+  'Remitly', 
+  'WellsFargo',
+  'Viamericas',
+  'PNB',
+  'BofA',
+  'PayPal'
+].each do |name|
   ServiceProvider.find_or_create_by(name: name)
 end
