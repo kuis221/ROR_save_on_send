f82b04cd1336050f75a903e1659b22d7baeb2beb
Alexey Artamonov <i@useruby.com>
2014-11-10 11:21:16 +0100

    not allow to user open welcome page directly #81908702

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+  before_filter :redirect_to_root
+
+  private
+  def redirect_to_root
+    redirect_to(root_path) if !user_signed_in? && flash.empty?
+  end
 end
