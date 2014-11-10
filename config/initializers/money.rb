c2383a7969efdb9deac0b45ec23b30c0f1df9671
Alexey Artamonov <i@useruby.com>
2014-11-10 17:11:38 +0100

    added pre-defined fx rate #82393314

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,4 +1,11 @@
 MoneyRails.configure do |config|
   # set the default currency
   config.default_currency = :usd
+
+  # FIXME remove hardcoded exchange rate when we'll implement getting rate 
+  # from online service
+  config.add_rate 'USD', 'CNY', 6.12
+  config.add_rate 'USD', 'INR', 61.40
+  config.add_rate 'USD', 'MXN', 13.61
+  config.add_rate 'USD', 'PHP', 45.01
 end
