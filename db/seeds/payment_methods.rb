69e0ef8be9b58b4561ec38d281634e3d49637099
Alexey Artamonov <i@useruby.com>
2014-11-11 17:27:51 +0100

    added plastic card into seed #82043862

--- db/seeds/payment_methods.rb
+++ db/seeds/payment_methods.rb
@@ -1,3 +1,3 @@
-{'cash' => 'cash', 'bank account' => 'bank'}.each do |name, slug|
+{'cash' => 'cash', 'bank account' => 'bank', 'plastic card' => 'card'}.each do |name, slug|
   PaymentMethod.create_with(name: name).find_or_create_by(slug: slug)
 end
