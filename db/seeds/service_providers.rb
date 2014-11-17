abbd7fbe5d17a364aa2f767757fc262c2a2d0bc2
Alexey Artamonov <i@useruby.com>
2014-11-17 09:23:41 +0100

    update remittance terms | added wallmart provider #82808566

--- db/seeds/service_providers.rb
+++ db/seeds/service_providers.rb
@@ -7,10 +7,11 @@
   ['Transferwise', 'https://transferwise.com/request/placeOrder?execution=e1s2'],
   ['Remitly', 'https://www.remitly.com/help/view/87'],
   ['WellsFargo', 'https://www.wellsfargo.com/international-remittances/cost-estimator'],
-  ['Viamericas', ''],
+  ['Viamericas', 'https://www.viamericas.com/CustomerDirect/compareprices'],
   ['PNB', 'https://pnbwebremit.com/Fees.aspx'],
   ['BofA', 'https://www.bankofamerica.com/foreign-exchange/wire-transfer.go'],
-  ['PayPal', 'https://www.paypal.com/us/cgi-bin/webscr?cmd=xpt/Marketing_CommandDriven/general/International_Money_Transfer-outside']
+  ['PayPal', 'https://www.paypal.com/us/cgi-bin/webscr?cmd=xpt/Marketing_CommandDriven/general/International_Money_Transfer-outside'],
+  ['WalMart', 'https://walmart.moneygram.com/MGOWap/flows/home?execution=e1s1#costEstimator']
 ].each do |provider_attrs|
   service_provider = ServiceProvider.find_or_create_by(name: provider_attrs[0])
   service_provider.update_attribute(:landing_page, provider_attrs[1])
