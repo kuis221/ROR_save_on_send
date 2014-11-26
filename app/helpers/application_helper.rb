df5ea80aabeadc12e90afa0108c4120802d41d47
Alexey Artamonov <i@useruby.com>
2014-11-26 13:11:59 +0100

    round-up decimals and delete pre-set zeroes #83262588

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def money_field(form, resource, field_name)
+    value = resource.send(field_name)
+    
+    if value > 0
+      form.text_field(field_name, 
+                      class: 'form-control', 
+                      value: money_without_cents(value)
+                     )
+    else
+      form.text_field(field_name, class: 'form-control', value: '', placeholder: '0')
+    end
+  end
 end
