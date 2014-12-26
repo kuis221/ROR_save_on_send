20015ea97c900f9f0c8066eb18c8c06e05348241
Alexey Artamonov <i@useruby.com>
2014-12-26 12:23:25 +0100

    leave only email field in the registration form #84936786

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
+  PHONE_REGEX = /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/
+  EMAIL_REGEX = /\A([-a-z0-9!\#$%&'*+\/=?^_`{|}~]+\.)*[-a-z0-9!\#$%&'*+\/=?^_`{|}~]+@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :omniauthable, omniauth_providers: [:facebook],
+         authentication_keys: [:login]
 
   belongs_to :money_transfer_destination, class_name: Country
 
@@ -16,9 +20,22 @@ class User < ActiveRecord::Base
   has_many :referrals
 
   # validations
-  validates_presence_of :first_name
-  validates_presence_of :zipcode, unless: :skip_additional_info?
-  validates_presence_of :money_transfer_destination, unless: :skip_additional_info?
+  #validates_presence_of :first_name
+  #validates_presence_of :zipcode, unless: :skip_additional_info?
+  #validates_presence_of :money_transfer_destination, unless: :skip_additional_info?
+
+  validates :phone, uniqueness: {case_sensitive: false}, format: {with: PHONE_REGEX}, 
+    presence: true, if: :email_blank?
+
+  before_validation :fill_phone, on: :create
+
+  def password_required?
+    super if confirmed?
+  end
+
+  def email_required?
+    phone_blank?
+  end
 
   def prefered_currency
     money_transfer_destination.try(:receive_currency)
@@ -40,6 +57,19 @@ class User < ActiveRecord::Base
     zipcode.present? && money_transfer_destination.present?
   end
 
+  def phone_blank?
+    phone.blank?
+  end
+
+  def email_blank?
+    email.blank?
+  end
+
+  # FIXME we not send confirmation instruction while we don't have custom confirmation
+  # page
+  def send_confirmation_instructions
+  end
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -48,6 +78,14 @@ class User < ActiveRecord::Base
       user.last_name = auth.info.last_name
       user.skip_confirmation!
       user.skip_additional_info!
+    end
+  end
+
+  private
+  def fill_phone
+    if email.match(PHONE_REGEX)
+      self.phone = email
+      self.email = ''
     end
   end
 end
