ff045af2d9a3a3301491c1885b674cf89a9554f8
Alexey Artamonov <i@useruby.com>
2015-01-09 20:51:27 +0100

    removed devise flash notice from welcome page #85179726

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   before_filter :redirect_to_root
 
+  def index
+    # we don't need to display devise message on this page
+    flash[:notice] = nil
+  end
+
   private
   def redirect_to_root
     redirect_to(root_path) if !user_signed_in? && flash.empty?
