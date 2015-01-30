c21e4ef4031562fa5115678e510e09bfcddb489b
m8ig <ignatyev@sitio.ru>
2015-01-30 03:06:56 +0100

    Delete useless code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,6 @@
 //= require vendors/jquery.tablesaw
 
 //= require bootstrap
-//= require user_next_transfer
+// require user_next_transfer
 //= require user_rencent_transaction
 //= require form_controller
