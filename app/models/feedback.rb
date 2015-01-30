f1348472e6d1b728a28aeef156d7f790bdc9d66a
Alexey Artamonov <i@useruby.com>
2015-01-30 11:54:01 +0100

    added three different error message for blank field in the user feedback #86613634

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,7 +2,25 @@ class Feedback < ActiveRecord::Base
   belongs_to :commendable, polymorphic: true
   belongs_to :user
 
-  validates_presence_of :service_quality, :comments
+  validate :service_quality_and_comments
+
+  protected
+  def service_quality_and_comments
+    
+    if service_quality.nil? || comments.blank?
+      err_msg = if service_quality.nil? && comments.blank?
+                  'service_quality_and_comments.blank'
+                elsif service_quality.nil?
+                  'service_quality.blank'
+                elsif comments.blank?
+                  'comments.blank'
+                end
+
+      errors.add(:service_quality_and_comments, 
+                 I18n.t(err_msg, scope: 'activerecord.errors.models.feedback.attributes')
+      )
+    end
+  end
 
   # rails admin configuration
   rails_admin do
