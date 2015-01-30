741fd641952d51bcadd196db9a7f87f46c2e00f8
m8ig <ignatyev@sitio.ru>
2015-01-30 03:07:37 +0100

    Choose country by click on flag

--- app/assets/javascripts/form_controller.js
+++ app/assets/javascripts/form_controller.js
@@ -11,4 +11,34 @@ $(document).ready(function() {
 	    showToday: true,
 	    pickTime: false
 	});
+
+
+
+    $('[name="user_next_transfer[receive_currency]"]').click(function() {
+    	$('#amount_receive_currency').html($('<i class="fa fa-fw fa-'+$(this).val().toLowerCase()+'">'));
+    });
+
+
+    $('[href="#apply_payment_methods"]').click(function() {
+		$('#user_next_transfer_originating_source_of_funds_id_' + $('.send_method_id', $(this).parent()).text()).attr('checked', true);
+		$('#user_next_transfer_destination_preference_for_funds_id_' + $('.receive_method_id', $(this).parent()).text()).attr('checked', true);
+		$('form.user_next_transfer').submit();
+    });
+
+
+    $('[name="user_next_transfer[money_transfer_destination_id]"]').change(function(event) {
+		var country = $(this).data('country');
+		  
+		$('#amount_receive_currency').html('');
+		
+		if (country == 'india') {
+			$('#amount_receive_currency').html($('<i class="fa fa-fw fa-inr">'));	  	
+		}
+		if (country == 'mexico') {
+			$('#amount_receive_currency').html($('<i class="fa fa-fw fa-mxn">'));	  	
+		}
+		  
+		$('.receive_currency').addClass('hidden');
+		$('#receive_currency_' + country).removeClass('hidden');
+    });
 });
