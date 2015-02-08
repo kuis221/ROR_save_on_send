10af1acb0e99a68771d3d202f0410639ea60b543
Alexey Artamonov <i@useruby.com>
2015-02-08 22:47:40 +0100

    add ssl for all devise controllers #87689604

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # force ssl for devise controllers
+  config.to_prepare { Devise::SessionsController.force_ssl }
+  config.to_prepare { Devise::RegistrationsController.force_ssl }
+  config.to_prepare { Devise::PasswordsController.force_ssl }
 end
