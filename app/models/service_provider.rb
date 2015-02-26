e043ae43627cc58dc0fae5fad485abf2abdae963
Alexey Artamonov <i@useruby.com>
2015-02-26 21:06:06 +0100

    change order for feedbacks on provider page #88698656

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -3,14 +3,14 @@ class ServiceProvider < ActiveRecord::Base
   friendly_id :name, use: :slugged
 
   has_many :feedbacks, ->{where(approved: true)}, as: :commendable
-  has_many :recent_transactions, inverse_of: :service_provider, class_name: User::RecentTransaction
+  has_many :recent_transactions, ->{order(date: :desc)}, inverse_of: :service_provider, class_name: User::RecentTransaction
 
   belongs_to :created_by, polymorphic: true
 
   scope :created_by_admin, ->{where(created_by: Admin.all)}
 
   def display_feedbacks
-    @display_feedbacks ||= all_feedbacks.sort{|feedback_a, feedback_b| feedback_b.created_at <=> feedback_a.created_at}
+    @display_feedbacks ||= all_feedbacks
   end
 
   def average_rating
@@ -24,8 +24,7 @@ class ServiceProvider < ActiveRecord::Base
   end
 
   def all_feedbacks
-    recent_transactions.joins(:feedback).where('feedbacks.approved is TRUE').map(&:feedback) + 
-    feedbacks
+    recent_transactions.joins(:feedback).where('feedbacks.approved is TRUE').map(&:feedback) 
   end
 
   def self.for_country(country)
