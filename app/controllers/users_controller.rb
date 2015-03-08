ce87d5838861b0063b441b77b2a03de57febc370
m8ig <ignatyev@sitio.ru>
2015-03-08 11:02:40 +0400

    Deficiences with avatar upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,20 +14,10 @@ class UsersController < ApplicationController
 
     @user.update_attributes(params.require(:user).permit(:first_name, :last_name, :zipcode, :about_me))
 
-      format.json do
-        if @user.avatar
-          render json: {
-                        name: @user.full_name, 
-                        size: @user.avatar.size, 
-                        url: @user.avatar_url,
-                        thumbnail_url: @user.avatar_url,
-                        delete_url: user_avatar_path,
-                        delete_type: 'DELETE'
-          }
-        else
-          render json: [{error: "custom_failure"}], status: 304
-        end
-      end
+    if @user.valid?
+      redirect_to(root_path)
+    else
+      render(:edit)
     end
   end
 end
