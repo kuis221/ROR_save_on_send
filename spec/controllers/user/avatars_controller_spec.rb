4d0adf31da59b133249d5d3ba5d5e87009220ff8
Alexey Artamonov <i@useruby.com>
2015-03-16 15:13:01 +0100

    fixed broken specs #84205730

--- spec/controllers/user/avatars_controller_spec.rb
+++ spec/controllers/user/avatars_controller_spec.rb
@@ -2,28 +2,22 @@ require 'spec_helper'
 
 describe AvatarsController do
   let(:user){FactoryGirl.create(:confirmed_user)}
-  let(:avatar_path){Rails.root.join('spec', 'fixtures', 'files', 'avatar.png')}
 
-  before do
-    Cloudinary.config do |config|
-      config.cloud_name = 'test_cloud'
-    end
-
-    allow_any_instance_of(AvatarUploader).to receive(:store!)
-    allow(Cloudinary::Uploader).to receive(:destroy)
+  before(:each) do
+    allow(Cloudinary::Uploader).to receive(:destroy)  
   end
 
   describe 'DELETE destroy' do
     it 'should delete avatar' do
       sign_in(user)
 
-      user.update_attribute(:avatar, File.open(avatar_path))
+      user.update_attribute(:avatar, 'public_id')
 
       delete :destroy, format: :json
 
       expect(response).to be_ok
      
-      expect(assigns[:current_user].avatar.url).to be_nil
+      expect(assigns[:current_user].avatar).to be_nil
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,29 +2,19 @@ require 'spec_helper'
 
 describe UsersController do
   let(:user){FactoryGirl.create(:confirmed_user)}
-  let(:avatar_path){Rails.root.join('spec', 'fixtures', 'files', 'avatar.png')}
 
-  before do
-    Cloudinary.config do |config|
-      config.cloud_name = 'test_cloud'
-    end
-
-    allow_any_instance_of(AvatarUploader).to receive(:store!)
-    allow(Cloudinary::Uploader).to receive(:destroy)
+  before :each do
+    allow_any_instance_of(Cloudinary::PreloadedFile).to receive(:valid?).and_return(true)
   end
 
   describe 'PATCH update' do
     it 'should upload avatar' do
       sign_in(user)
 
-      patch :update, format: :json, user: {avatar: fixture_file_upload(avatar_path, 'image/png')}
+      patch :update, format: :json, avatar: 'upload/image/v123/public_id.jpg#321'
 
       expect(response).to be_ok
-      
-      upload_response = JSON.parse(response.body)
-      expect(upload_response['name']).to be_present
-      expect(upload_response['size']).to be_present
-      expect(upload_response['url']).to be_present
+      expect(response.body).to eq('true')
     end
   end
 end
