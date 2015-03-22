c6c9c18a9784a4c81aaf21e9838ecb5c5ab28dc9
Alexey Artamonov <i@useruby.com>
2015-03-22 15:48:28 +0100

    underscores in the URLs replaced with hyphens #86377030

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,34 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
  
-  as :user do
-    patch '/user/confirmation' => 'confirmations#update', via: :patch, as: :update_user_confirmation
-  end
-
-  devise_for :users, controllers: { 
+  devise_for :users, skip: [:sessions, :registrations], controllers: { 
     registrations: 'registrations',
     confirmations: 'confirmations',
     omniauth_callbacks: 'omniauth_callbacks'
   }
+
+  as :user do
+    patch '/user/confirmation' => 'confirmations#update', via: :patch, as: :update_user_confirmation
+    get 'signin' => 'devise/sessions#new', as: :new_user_session
+    post 'signin' => 'devise/sessions#create', as: :user_session
+    get 'signup' => 'registrations#new', as: :new_user_registration
+    post 'signup' => 'registrations#create', as: :user_registration
+    delete 'signout' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
   
   resource :user, only: [:edit, :update] do
     resource :avatar, only: [:destroy]
   end
 
-  resource :user_recent_transaction, only: [:new, :create]
-  resource :user_next_transfers, only: [:new, :show, :create]
-
+  resource 'user-recent-transaction', 
+    only: [:new, :create], 
+    controller: :user_recent_transactions, 
+    as: :user_recent_transaction
+  resource 'user-next-transfers', 
+    only: [:new, :show, :create],
+    controller: :user_next_transfers,
+    as: :user_next_transfers
+  
   resources :service_providers, only: [:show], path: 'providers'
 
   resources :feedbacks, only: [:create]
