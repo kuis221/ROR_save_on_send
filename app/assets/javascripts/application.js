ef51bf234b737b2d8e17f06dd5c68c364dbdc585
m8ig <ignatyev@sitio.ru>
2015-04-08 20:59:35 +0200

    Sharing buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,6 +24,7 @@
 //= require vendors/jquery.mask.min
 //= require vendors/jquery.rating.pack
 //= require vendors/jquery.tablesaw
+//= require vendors/rrssb
 
 //= require bootstrap
 //= require form_controller
